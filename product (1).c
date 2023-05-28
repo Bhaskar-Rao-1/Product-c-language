ce85c7128a747c9b9b7acc0a03a131744b85ebf2
Bhaskar-Rao-1 <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-05-28 19:11:36 +0530

    Add files via upload

---
+++ product (1).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("###multiplying  floating numbers###\n");
+    float a=9.8494,b=75.73838;
+    printf("\n");
+    printf("product of two numbers %f",a*b);
+    
+    return 0;
+}
